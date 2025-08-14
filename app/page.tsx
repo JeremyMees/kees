05fd683ed47b52ec4752ce1b399407785e3a856f
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-08-14 18:32:43 +0200

    feat: confirm dialog for removing food

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 import { foodsCollection, create, update, remove } from '@/lib/firebase.browser'
 import { onSnapshot } from 'firebase/firestore'
 import { Food } from '@/types/firebase'
+
+import FoodForm from '@/components/Forms/Food'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
-import { X } from 'lucide-react'
-import FoodForm from '@/components/Forms/Food'
-import { useEffect, useState } from 'react'
 import { NumberInput } from '@/components/ui/number-input'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { X } from 'lucide-react'
 
 export default function Page() {
   const [foods, setFoods] = useState<Food[]>([])
+  const [selectedFood, setSelectedFood] = useState<Food>()
   const [loading, setLoading] = useState(true)
   const [list] = useAutoAnimate()
 
@@ -32,16 +46,44 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-6 content pt-2 max-h-full overflow-y-auto">
       <FoodForm onAdd={(food) => create(foodsCollection, food)} />
-      <ul ref={list} className="flex flex-col pb-8 overflow-y-auto">
-        {loading
-          ? Array.from({ length: 10 }).map((_, i) => <FoodSkeleton key={i} />)
-          : foods.map((food) => <FoodItem key={food.id} food={food} />)}
-      </ul>
+      <AlertDialog>
+        <ul ref={list} className="flex flex-col pb-8 overflow-y-auto">
+          {loading
+            ? Array.from({ length: 10 }).map((_, i) => <FoodSkeleton key={i} />)
+            : foods.map((food) => (
+                <FoodItem
+                  key={food.id}
+                  food={food}
+                  onDelete={() => setSelectedFood(food)}
+                />
+              ))}
+        </ul>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Remove {selectedFood?.item}?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setSelectedFood(undefined)}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                if (selectedFood?.id) remove(foodsCollection, selectedFood.id)
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
 
-function FoodItem({ food }: { food: Food }) {
+function FoodItem({ food, onDelete }: { food: Food; onDelete: () => void }) {
   return (
     <li className="grid grid-cols-7 items-center gap-2 border-b text-sm py-1">
       <NumberInput
@@ -56,14 +98,16 @@ function FoodItem({ food }: { food: Food }) {
       <span className="font-semibold text-ellipsis line-clamp-1 col-span-4">
         {food.item}
       </span>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="justify-self-end"
-        onClick={() => remove(foodsCollection, food.id)}
-      >
-        <X />
-      </Button>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="justify-self-end"
+          onClick={onDelete}
+        >
+          <X />
+        </Button>
+      </AlertDialogTrigger>
     </li>
   )
 }
